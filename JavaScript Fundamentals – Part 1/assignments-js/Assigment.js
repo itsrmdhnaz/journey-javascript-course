aca64dda0403d257f4b0c0f95707344af71e1353
itsrmdhnaz <ramadhan.abdul.aziz14@gmail.com>
2023-06-30 01:20:21 +0700

    repair structure assigment one and two clear

--- JavaScript Fundamentals – Part 1/assignments-js/Assigment.js
+++ JavaScript Fundamentals – Part 1/assignments-js/Assigment.js
@@ -1,12 +1,24 @@
-//LECTURE: Data Types
+////////////////////////////////////////////////
+//LECTURE: Values and Variables
 
-//Variables from assignment one
+//Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
 let country = 'Indonesia'; //string value ('')
  let continent = 'Asia'; //string value ('')
- let population = 10; //integer value because 10 is a number
+ let population = 200; //integer value because 10 is a number
+
+
+ //Log their values to the console
+ console.log(country); //indonesia
+ console.log(continent); //Asia
+ console.log(population); //200 means 200 million population
+ // end LECTURE: Values and Variables
+ ////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+//LECTURE: Data Types
 
 //Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
-let isIsland = true;
+let isIsland = true; //boolean value, because (true)
  let language; //just declaration variable will output undefined
 
  //Log the types of 'isIsland', 'population', 'country' and 'language' to the console
@@ -14,3 +26,6 @@ let isIsland = true;
  console.log(typeof population); //integer value, because population value is (10) that is integer
  console.log(typeof country);  //string value, because country value is ('Indonesia') that is string value
  console.log(typeof language); //undefined value, because language value haven't been assigned yet
+
+ // end of LECTURE: Data Types
+ ////////////////////////////////////////////////
