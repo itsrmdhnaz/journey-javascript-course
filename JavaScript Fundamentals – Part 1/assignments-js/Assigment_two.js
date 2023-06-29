032b3d154fe1a74c7af63d7fa80e4339cf9a4983
itsrmdhnaz <ramadhan.abdul.aziz14@gmail.com>
2023-06-30 01:02:01 +0700

    Assigment_two_complete

---
+++ JavaScript Fundamentals – Part 1/assignments-js/Assigment_two.js
@@ -0,0 +1,16 @@
+//LECTURE: Data Types
+
+//Variables from assignment one
+let country = 'Indonesia';
+ let continent = 'Asia';
+ let population = 10;
+
+//Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+let isIsland = true;
+ let language; //just declaration variable will output undefined
+
+ //Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+ console.log(typeof isIsland); //boolean value because isIsland value is true that is boolean value
+ console.log(typeof population); //integer value because population value is integer
+ console.log(typeof country);  //string value because country value is string
+ console.log(typeof language); //undefined value because language value haven't been assigned yet
