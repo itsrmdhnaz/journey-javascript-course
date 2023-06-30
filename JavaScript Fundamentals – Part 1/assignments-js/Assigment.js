a4593183b296c64cf75f35e0025a2c16435d7867
itsrmdhnaz <ramadhan.abdul.aziz14@gmail.com>
2023-07-01 00:11:43 +0700

    Taking Decisions: if / else Statements done

--- JavaScript Fundamentals – Part 1/assignments-js/Assigment.js
+++ JavaScript Fundamentals – Part 1/assignments-js/Assigment.js
@@ -60,11 +60,11 @@ const isIsland = true;
 //LECTURE: Basic Operators
 
 //If your country split in half, and each half would contain half the population, then how many people would live in each half?
-const halfPopulation = population/2; //why devided by two because want to get the half population 1/2
+const halfPopulation = population / 2; //why devided by two because want to get the half population 1/2
 console.log(halfPopulation); //console the result
 
 //Increase the population of your country by 1 and log the result to the console
-population++; 
+population++;
 console.log(population); //console the result
 
 //Finland has a population of 6 million. Does your country have more people than Finland?
@@ -76,18 +76,55 @@ const averagePopulation = 33;
 console.log(population < averagePopulation); //false because indonesia have more people than the average population of the country
 
 //Based on the variables you created, create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'
-let description = country + ' is in ' + continent + ' and its ' + population + ' million people speak ' + language;
-console.log(description); 
+let description =
+  country +
+  " is in " +
+  continent +
+  " and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
 
 // end of LECTURE: Basic Operators
 ////////////////////////////////////////////////
 
-
 ////////////////////////////////////////////////
 //LECTURE: Strings and Template Literals
 
-description = `${country} is in ${continent} and its ${population} million people speak ${language}`
+description = `${country} is in ${continent} and its ${population} million people speak ${language}`;
 console.log(description);
 
 // end of LECTURE: Strings and Template Literals
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// LECTURE: Taking Decisions: if / else Statements
+
+//If your country's population is greater that 33 million, log a string like this to the console: 'Portugal's population is above average'. Otherwise, log a string like 'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population)
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(`${country}'s population is  ${33 - population} million below average`);
+}
+
+// After checking the result, change the population temporarily to 13 and then to
+// 130. See the different results, and set the population back to original
+population = 13;
+population = 130;
+
+// end of LECTURE: Taking Decisions
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+//
+
+//
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+//
+
+//
 ////////////////////////////////////////////////
