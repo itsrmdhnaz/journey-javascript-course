89a719d6b72526d230f301c9dd1f6291f390554a
itsrmdhnaz <ramadhan.abdul.aziz14@gmail.com>
2023-07-18 18:52:58 +0700

    05-Guess-My-Number finsih

---
+++ 03_Developer-Skills/Challenge.js
@@ -0,0 +1,31 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = (arr) => {
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    result += `... ${arr[i]}ºC in ${i + 1} days`;
+  }
+  return result + "...";
+};
+
+console.log(printForecast(data1));
+console.log(printForecast(data2));
+
+/**
+ * my another solving function for the challenge
+ 
+const printForecast = (arr) => {
+  return (
+    arr.map((item, index) => `... ${item}ºC in ${index + 1} days`).join("") +
+    "..."
+  );
+};
+
+console.log(printForecast(data1));
+console.log(printForecast(data2));
+
+*/
---
+++ 05-Guess-My-Number/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let higherScore = 0;
+document.querySelector(".number").textContent = secretNumber;
+
+const changeText = (element, text) => {
+  document.querySelector(element).textContent = text;
+};
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+
+  !guess
+    ? changeText(".message", "🚫 No Number!")
+    : guess === secretNumber
+    ? (changeText(".message", "👏🏻 Correct Number!"),
+      (document.querySelector("body").style.backgroundColor = "#60b347"),
+      (document.querySelector(".number").style.width = "30rem"),
+      changeText(".number", secretNumber),
+      score > higherScore
+        ? ((higherScore = score), changeText(".highscore", higherScore))
+        : null)
+    : guess !== secretNumber
+    ? score > 1
+      ? (changeText(
+          ".message",
+          guess > secretNumber ? "Too High bruh!" : "Too Low bruh!"
+        ),
+        score--,
+        changeText(".score", score))
+      : (changeText("score", 0), changeText(".message", "You lost the game:("))
+    : null;
+});
+
+document.querySelector(".again").addEventListener("click", () => {
+  const number = document.querySelector(".number");
+  const body = document.querySelector("body");
+  const guess = document.querySelector(".guess");
+
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+
+  changeText(".score", score);
+  guess.value = "";
+  number.textContent = secretNumber;
+  number.style.width = "15rem";
+  body.style.backgroundColor = "#222";
+  changeText(".message", "Start guessing...");
+});
